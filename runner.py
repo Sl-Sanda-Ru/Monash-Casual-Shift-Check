dd97496cfd16d926e71a7b44eaeec86af2dea4a7
Sandaru Ashen <sandaruashen02@gmail.com>
2024-06-26 17:07:48 +1000

    Small feature add and bugfix

--- runner.py
+++ runner.py
@@ -1,5 +1,5 @@
 import requests
-from datetime import datetime
+from datetime import datetime, timedelta
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
@@ -27,8 +27,8 @@ options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 options.add_experimental_option("excludeSwitches", ["enable-logging"])
 
-nrml_msg = "You Have Done {} Hours, and You Got Future Bookings For {} Hours, You Can Do {} More Hours"
-exceed_msg = "You Have Done {} Hours, and You Got Future Bookings For {} Hours, **Hours Exceeded By {}**"
+nrml_msg = "You Have Done {} Hours, and You Got Future Bookings For {} Hours, You Can Do {:.2f} More Hours"
+exceed_msg = "You Have Done {} Hours, and You Got Future Bookings For {} Hours, **Hours Exceeded By {:.2f}**"
 
 
 handlerclient = Client(
@@ -43,7 +43,6 @@ senderclient = Client(
     api_id=getenv("APIID"),
     api_hash=getenv("APIHASH"),
 )
-senderclient.start()
 logging.info("Telegram Bot Clients Created")
 
 
@@ -70,8 +69,8 @@ def calculate_shift_hours(shift_start, shift_end):
     return (end_time - start_time).total_seconds() / 3600  # to get in hours
 
 
-def process_roster_data(roster_data):
-    shifts = []
+def process_roster_data(roster_data, fortnight_end):
+    shifts = ([], [])  # shifts within the fortnight, shifts outside fortnight
     total_hours = 0
     current_date = datetime.now().date()
     current_time = datetime.now().time()
@@ -88,20 +87,30 @@ def process_roster_data(roster_data):
         else:
             location += f" {assigner}"
 
-        if shift_date > current_date or (
-            shift_date == current_date
-            and not datetime.strptime(shift_end, "%H:%M").time() < current_time
+        if (
+            shift_date < fortnight_end
+            and shift_date > current_date
+            or (
+                shift_date == current_date
+                and not datetime.strptime(shift_end, "%H:%M").time() < current_time
+            )
         ):
-            shifts.append(f"{shift_date} {location} {shift_start} {shift_end}")
+            shifts[0].append(f"{shift_date} {location} {shift_start} {shift_end}")
             total_hours += calculate_shift_hours(shift_start, shift_end)
+        elif shift_date > fortnight_end:
+            shifts[1].append(f"{shift_date} {location} {shift_start} {shift_end}")
 
     return shifts, total_hours
 
 
 def get_completed_hours():
-    driver = webdriver.Chrome(
-        service=Service(ChromeDriverManager().install()), options=options
-    )
+    if getenv("CHRMDRIVR"):
+        cService = webdriver.ChromeService(executable_path=getenv("CHRMDRIVR"))
+        driver = webdriver.Chrome(service=cService, options=options)
+    else:
+        driver = webdriver.Chrome(
+            service=Service(ChromeDriverManager().install()), options=options
+        )
     driver.get(
         "https://cust03-prd01-ath01.prd.mykronos.com/authn/XUI/?realm=monashhealth_prd_01&service=1850CustomerIDPChain&goto=https%3A%2F%2Fmonashhealth-sso.prd.mykronos.com%3A443%2F"
     )
@@ -120,6 +129,7 @@ def get_completed_hours():
     driver.get(
         "https://monashhealth-sso.prd.mykronos.com/timekeeping#/myTimecard?ctxt=myTimecard"
     )
+    sleep(5)
     hours = (
         WebDriverWait(driver, 15)
         .until(
@@ -129,27 +139,38 @@ def get_completed_hours():
         )
         .text
     )
+    fortnight_end = driver.find_element(
+        By.XPATH, '//*[@id="0_date"]/span[3]/span/span'
+    ).text
+    fortnight_end = (
+        datetime.strptime(fortnight_end, "%a %d/%m").replace(year=datetime.now().year)
+        + timedelta(days=14)
+    ).date()
     driver.quit()
-    logging.info(f"get_completed_hours: {hours}")
-    return float(hours)
+    logging.info(f"get_completed_hours: {hours} fortnight end:{fortnight_end}")
+    return float(hours), fortnight_end
 
 
 def generate_report():
-    rosterdata = process_roster_data(fetch_roster_data())
     while True:
         try:
-            donehours = get_completed_hours()
+            donehours, fortnight_end = get_completed_hours()
         except ValueError:
             sleep(15)
         else:
             break
-
+    rosterdata = process_roster_data(fetch_roster_data(), fortnight_end)
     total = rosterdata[1] + donehours
     if total > 48:
         msg = exceed_msg.format(donehours, rosterdata[1], total - 48)
     else:
         msg = nrml_msg.format(donehours, rosterdata[1], 48 - total)
-    return f"{msg}\nHere's Your Schedule:\n" + "\n".join(rosterdata[0])
+    return (
+        f"{msg}\n\nHere's Your Schedule:\n"
+        + "\n".join(rosterdata[0][0])
+        + "\n\nShifts booked for upcoming fortnights:\n"
+        + "\n".join(rosterdata[0][1])
+    )
 
 
 def sender():
@@ -173,10 +194,11 @@ def check(client, message):
 
 
 scheduler_thread = Thread(target=run_scheduler)
-scheduler_thread.start()
+
 try:
+    scheduler_thread.start()
     handlerclient.run()
+    senderclient.start()
 except KeyboardInterrupt:
     logging.info("Script terminated by user")
-finally:
-    senderclient.stop()
+    exit()
